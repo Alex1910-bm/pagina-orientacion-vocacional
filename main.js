179b296a0d7e53ca406679014e1089f4b283b563
Alex1910-bm <62450105+Alex1910-bm@users.noreply.github.com>
2020-09-18 20:27:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show")
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.Introduction', { delay: 500 });
+ScrollReveal().reveal('.themes', { delay: 500 });
+ScrollReveal().reveal('.credits', { delay: 500 });
---
+++ mainI.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show")
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.factores', { delay: 500 });
+ScrollReveal().reveal('.primer', { delay: 500 });
+ScrollReveal().reveal('.segundo', { delay: 500 });
+ScrollReveal().reveal('.tercera', { delay: 500 });
+ScrollReveal().reveal('.cuarta', { delay: 500 });
+ScrollReveal().reveal('.tomardecicines', { delay: 500 });
